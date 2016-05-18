45da9c38804f20bb82faf8d2536b2a4533b56087
tsam030 <tsam030@tecnicosuperiorekennedy.it>
2016-05-18 11:41:41 +0200

    ciaone

--- app/src/main/java/com/neko/noteapplication/MainActivity.java
+++ app/src/main/java/com/neko/noteapplication/MainActivity.java
@@ -57,70 +57,20 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         int ora= c.get(Calendar.HOUR_OF_DAY);
 
         if(ora>=20 || ora<5){
-            RelativeLayout rl = (  RelativeLayout )findViewById(R.id.schermo);
+            RelativeLayout rl = (RelativeLayout)findViewById(R.id.schermo);
             rl.setBackgroundColor(Color.parseColor("#232323"));}
 
 
 
         ListView listView = (ListView)findViewById(R.id.listView);
         List list = dataProvider.getListArray();
-
+        listView.setDivider(null);
         list.add(new Note("title","description","22"));
         list.add(new Note("title 2","description 2",":)"));
 
         CustomAdapter adapter = new CustomAdapter(this, R.layout.lista, list);
         listView.setAdapter(adapter);
 
-
-        //ArrayList<Note> m_data = dataProvider.getDataArray();
-
-       /* //begin init test data
-        final ArrayList<HashMap<Str1ing, Object>> m_data = new ArrayList<HashMap<String, Object>>();
-
-        for(int i=0;i<100;i++){
-            HashMap<String, Object> map4 = new HashMap<String, Object>();
-            map3.put("maintext", "Cucina");
-            map3.put("subtext", "comprare pomodori");
-            map3.put("day", "26");
-            m_data.add(map3);
-        }
-
-        final ListView lv = (ListView) m_this.findViewById(R.id.listView);
-        lv.setDivider(null);
-        lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-
-        adapter = new SimpleAdapter(m_this,
-                m_data,
-                R.layout.lista,
-                new String[] {"maintext", "subtext", "day"},
-                new int[] {R.id.tv_MainText, R.id.tv_SubText,R.id.day});
-
-        adapter.setViewBinder(new SimpleAdapter.ViewBinder()
-        {
-            public boolean setViewValue(View view, Object data, String textRepresentation)
-            {
-                if (data == null) //if 2nd line text is null, its textview should be hidden
-                {
-                    view.setVisibility(View.GONE);
-                    return true;
-                }
-                view.setVisibility(View.VISIBLE);
-                return false;
-            }
-
-        });
-        // Bind to our new adapter.
-        lv.setAdapter(adapter);
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View v, int arg2, long arg3) {
-
-    \
-                Toast.makeText(getApplicationContext(), "You have chosen : "+arg2,Toast.LENGTH_LONG).show();
-               //click listener
-            }
-        });
-        //show result*/
     }
 
     @Override
